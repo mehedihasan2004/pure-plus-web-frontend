3d98d6061a6a90741ba712eb9d68254e8a191c9f
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-04 23:11:55 +0600

    style: make carousel medium rounded

--- src/components/swiper/home-page-carousel.tsx
+++ src/components/swiper/home-page-carousel.tsx
@@ -11,14 +11,14 @@ export function HomePageCarousel() {
       modules={[Autoplay, Pagination]}
       pagination={{ dynamicBullets: true, clickable: true }}
       autoplay={{ delay: 5000, disableOnInteraction: true }}
-      className="h-[30vh] sm:max-md:h-[40vh] md:h-[60vh] xl:h-[80vh] select-none"
+      className="h-[30vh] sm:max-md:h-[40vh] md:h-[60vh] xl:h-[80vh] select-none rounded-md"
     >
       {Array(9)
         .fill(null)
         .map((_, i) => (
           <SwiperSlide
             key={i}
-            className="bg-cover"
+            className="bg-cover rounded-md"
             style={{
               backgroundImage: `url(/images/banner/${i + 1}.jpg)`,
               backgroundRepeat: 'no-repeat',
