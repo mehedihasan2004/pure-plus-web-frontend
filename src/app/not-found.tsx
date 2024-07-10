07a9bbbf6aeeec959da9076a993cd5d07f4442f6
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-10 21:35:51 +0600

    chore: indent a line of code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@nextui-org/react';
-import Link from 'next/link';
 
 export default function NotFound() {
   return (
