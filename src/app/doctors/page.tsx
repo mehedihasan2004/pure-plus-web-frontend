6741dc43664f36fdd2d00878dbabc616b33273b2
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-15 11:43:43 +0600

    feat: fetch the doctors data

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,21 +1,36 @@
+import axios from 'axios';
+import { api } from '@/lib/api/constants';
 import { DoctorCard } from './doctor-card';
 import { SearchAndFilterSidebar } from './search-and-filter-sidebar';
 
-export default function Doctors() {
-  const doctors = Array(8).fill(null);
+export default async function Doctors() {
+  let doctors = null;
+
+  try {
+    const res = await axios.get(`${api}/doctors`);
+
+    doctors = res?.data?.data;
+  } catch (error) {
+    console.error('Error From Get All Doctors ->', error);
+  }
 
   return (
     <section className="md:grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-6 h-full">
       <SearchAndFilterSidebar />
 
       <div className="md:col-span-2 lg:col-span-3 xl:col-span-4">
-        <h3>Doctors</h3>
-
-        <div className="lg:grid grid-cols-2 gap-4">
-          {doctors.map((_, i: number) => (
-            <DoctorCard key={i} />
-          ))}
-        </div>
+        {doctors ? (
+          <>
+            <h3>Doctors</h3>
+            <div className="lg:grid grid-cols-2 gap-4">
+              {doctors.map((_: any, i: number) => (
+                <DoctorCard key={i} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <h5>Sorry, no doctors found!</h5>
+        )}
       </div>
     </section>
   );
