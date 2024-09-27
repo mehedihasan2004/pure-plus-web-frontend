1349e8052f6436bbd72677ee803c0345ce358596
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-27 10:27:14 +0600

    feat: add tanstack query provider in global provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,9 +2,14 @@
 
 import { ReactNode } from 'react';
 import { ThemeProvider } from './theme-provider';
+import { TanstackQueryProvider } from './tanstack-query-provider';
 
 type Props = { children: ReactNode };
 
 export function Providers({ children }: Props) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <TanstackQueryProvider>{children}</TanstackQueryProvider>
+    </ThemeProvider>
+  );
 }
