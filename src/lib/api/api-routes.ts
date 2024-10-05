55eb04249d8112282ae0d71cb7e3bec4d5daac39
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-10-05 18:28:49 +0600

    feat: add server action for get a patient

--- src/lib/api/api-routes.ts
+++ src/lib/api/api-routes.ts
@@ -1,10 +1,20 @@
 const baseApi = process.env.NEXT_PUBLIC_MAIN_API as string;
 
 const doctorsRoute = '/doctors';
+const patientsRoute = '/patients';
 const appointmentsRoute = '/appointments';
 
 export const api = (() => {
   return {
+    // Patient routes
+    get patients() {
+      return {
+        get route() {
+          return baseApi + patientsRoute;
+        }
+      };
+    },
+
     // Doctor routes
     get doctors() {
       return {
